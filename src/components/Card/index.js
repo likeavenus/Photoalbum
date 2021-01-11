19d6a3cd6b020cd4321b6ec83efba92d93eb1b0c
Rafael Guliev <rafael_guliev94@list.ru>
2021-01-11 21:10:35 +0300

    master | add node-sass, update README.MD, create card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+export const Card = () => {
+    return (
+        <div className={styles.card}>Card component</div>
+    )
+}

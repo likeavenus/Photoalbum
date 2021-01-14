de3aa11b5496bb45d9ece68e38f0700449405606
Rafael Guliev <rafael_guliev94@list.ru>
2021-01-15 00:12:41 +0300

    add-firebase | add firebase, storage, add upload image logic, refactoring

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
 import './App.css';
 import { CardsContainer } from './components/CardsContainer';
 import { Upload } from './components/UploadFoto';
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import firebase from "firebase/app";
-// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
-// import * as firebase from "firebase/app"
 
-// If you enabled Analytics in your project, add the Firebase SDK for Analytics
-import "firebase/analytics";
-
-// Add the Firebase products that you want to use
-import "firebase/auth";
-import "firebase/firestore";
-import 'firebase/storage';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDcymeDWgOcaAsXDfY9SBZhmOn7744xERM",
-  authDomain: "album-c5591.firebaseapp.com",
-  databaseURL: "https://album-c5591-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "album-c5591",
-  storageBucket: "album-c5591.appspot.com",
-  messagingSenderId: "48004309131",
-  appId: "1:48004309131:web:36c50ce064fce365dff48e",
-  measurementId: "G-SF3RGPJQPJ"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-console.log(firebase)
 function App() {
   return (
     <div className="App">
--- src/components/UploadFoto/index.js
+++ src/components/UploadFoto/index.js
@@ -1,25 +1,48 @@
 import React, { useState } from 'react';
+import firebase from "firebase/app";
+import "firebase/storage";
 import styles from './styles.module.scss';
+
+
 
 export const Upload = () => {
    const [file, setFile] = useState(null);
+   const [uploadError, setUploadError] = useState(false);
+   const [uploadingState, setUploadingState] = useState(false);
+
+   const storageRef = firebase.storage().ref();
+   const metadata = {
+      contentType: 'image/jpeg'
+   };
 
    const onInputChange = (e) => {
       setFile(null);
       setFile(e.target.files[0]);
-
    }
 
    const onButtonClick = () => {
-
+      if (file) {
+         const uploadTask = storageRef.child(`images/${file.name}`).put(file, metadata);
+         uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, () => {
+            setUploadingState(true);
+         }, (error) => {
+            setUploadError(true);
+            throw new Error(error);
+         }, () => {
+            uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+               console.log('File available at', downloadURL);
+               setUploadingState(false);
+             });
+         })
+      }
    }
-   // console.log(file);
 
    return (
       <div>
          <h3>Загрузите фотографию</h3>
          <input type="file" onChange={onInputChange} />
          <button onClick={onButtonClick}>Отправить</button>
+         {uploadingState && <p>Загрузка</p>}
       </div>
    )
 }

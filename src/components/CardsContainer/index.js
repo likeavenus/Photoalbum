0459b69e6aa8a8fb263c62a6557e262eb3ec15d8
Rafael Guliev <rafael_guliev94@list.ru>
2021-01-19 23:44:35 +0300

    develop | repait get data function

--- src/components/CardsContainer/index.js
+++ src/components/CardsContainer/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import styles from './styles.module.scss';
-import catsImg from '../../assets/cats.jpg';
 import { Card } from '../Card';
 import { Popup } from '../Popup';
 import firebase from "firebase/app";
@@ -13,25 +12,16 @@ export const CardsContainer = () => {
       id: null
    });
    const storageRef = firebase.storage().ref();
-   const imgArray = [];
+   const listRef = storageRef.child('images/');
 
+   const fetchData = async () => {
+      const promise = await listRef.listAll();
+      const items = await promise.items;
+      return Promise.all(items.map(item => item.getDownloadURL().then((data) => data)));
+   }
 
    useEffect(() => {
-
-      const listRef = storageRef.child('images/');
-
-      listRef.listAll().then(function (res) {
-         res.items.forEach(function (itemRef) {
-            itemRef.getDownloadURL().then((data) => {
-               imgArray.push(data);
-            })
-            // All the items under listRef.
-         });
-      }).catch(function (error) {
-         // Uh-oh, an error occurred!
-      });
-      setPhotos(imgArray);
-      //console.log(photos);
+      fetchData().then(data => setPhotos(data));
    }, []);
 
    const showImg = (id) => {
@@ -46,14 +36,6 @@ export const CardsContainer = () => {
          id: null
       });
    }
-   console.log(photos);
-   // let photosArr = [];
-   // if (photosArr.length) {
-   // photosArr = photos.map((item, index) => {
-   //    return <Card key={index} imgUrl={item} openCard={() => showImg(index)} />
-   // });
-   // }
-   // console.log(photosArr);
 
    return (
       <section className={styles.container}>
